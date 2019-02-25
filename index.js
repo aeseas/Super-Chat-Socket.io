898d14e17ccba29fff3132cbdd0138de9503aaff
Aisha El Salawi <aisha.elsalawi@aol.com>
2019-02-25 14:11:41 +0100

    tralal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io').listen(http);
 
 app.use(express.static('public'))
 
