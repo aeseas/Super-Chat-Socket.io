f81e45e73019baea13b5932331a9c4e317e5eda7
Aisha El Salawi <aisha.elsalawi@aol.com>
2019-02-25 14:14:01 +0100

    update create server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = require('express')();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io').listen(http);
 
 app.use(express.static('public'))
