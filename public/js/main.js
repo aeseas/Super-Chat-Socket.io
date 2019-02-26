3dc14b45367bcde39523cd4bf619848aa3b1adf5
Aisha El Salawi <aisha.elsalawi@aol.com>
2019-02-26 09:59:26 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -29,15 +29,15 @@ $(function (e) {
 
 
     //NOG EEN ANDERE SITE
-    $('form').submit(function (e) {
-        e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
-    });
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
-    });
+    // $('form').submit(function (e) {
+    //     e.preventDefault(); // prevents page reloading
+    //     socket.emit('chat message', $('#m').val());
+    //     $('#m').val('');
+    //     return false;
+    // });
+    // socket.on('chat message', function (msg) {
+    //     $('#messages').append($('<li>').text(msg));
+    // });
 });
 
 
